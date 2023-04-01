d7a5b7a5129935d732cd24e5e3c2c83b2e382355
pepinadas <mmmunapregunta@gmail.com>
2023-04-01 17:32:03 -0600

    Agregando un poco del MenuUI con los Switch Case

--- Conteo/src/UI/UIMenu.java
+++ Conteo/src/UI/UIMenu.java
@@ -1,5 +1,7 @@
 package UI;
 
+import java.util.Scanner;
+
 public class UIMenu {
     public static void showMenu() {
         System.out.println("Welcome to our homework");
@@ -9,7 +11,17 @@ public class UIMenu {
         do {
             System.out.println("1.- Total character count of a plain text file");
             System.out.println("2.- Count of each of the different characters contained in a plain text file");
-            System.out.println("3.- ");
+            System.out.println("3.- Word count");
+            System.out.println("4.- Line count");
+
+            Scanner sc = new Scanner(System.in);
+            response = Integer.valueOf(sc.nextLine());
+
+            switch (response){
+                case 1:
+                    System.out.println("Your select Total Character count");
+                case 2:
+            }
 
         } while (response != 0);
     }
