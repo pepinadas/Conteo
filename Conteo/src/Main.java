f9eae3ddf997da4b5195e8e5546c56dcf180548e
pepinadas <mmmunapregunta@gmail.com>
2023-04-01 15:49:17 -0600

    Primer commit

---
+++ Conteo/src/Main.java
@@ -0,0 +1,8 @@
+import static UI.UIMenu.showMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        //Take the static class UIMenu to show a simple UI to interact with the user
+        showMenu();
+    }
+}
---
+++ Conteo/src/UI/UIMenu.java
@@ -0,0 +1,16 @@
+package UI;
+
+public class UIMenu {
+    public static void showMenu() {
+        System.out.println("Welcome to our homework");
+        System.out.println("Please select the correct option:");
+
+        int response = 0;
+        do {
+            System.out.println("1.- Total character count of a plain text file");
+            System.out.println("2.- Count of each of the different characters contained in a plain text file");
+            System.out.println("3.- ");
+
+        } while (response != 0);
+    }
+}
